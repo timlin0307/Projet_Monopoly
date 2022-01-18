cdb0da5675b8adb57390a756a6b48bfd1f9fa7b2
Sasaki Yamato <54052564+timlin0307@users.noreply.github.com>
2022-01-18 15:27:38 +0100

    Update Propriete.h

--- Propriete.h
+++ Propriete.h
@@ -1,7 +1,7 @@
 /*
  * Propriete.h
  *
- *  Created on: 18 déc. 2021
+ *  Created on: 18 dÃ©c. 2021
  *      Author: lucas
  */
 
@@ -21,18 +21,15 @@ using namespace std;
  *
  */
 
-class Propriete : public Case
-{
-protected :
+class Propriete : public Case {
+    protected :
 	int loyer;
 	int prixAchat;
 	string nom_joueur; /* nom du joueur a "" si personne n'a la case */
 	int val_hypotheque;
 	bool hypotheque;
 
-
-
-public :
+    public :
 	Propriete(string nomcase, int loyer, int prixachat, string nomjoueur, int valhypo, bool hypotheque = false );
 	void setLoyer(int);
 	int getLoyer();
@@ -42,17 +39,14 @@ public :
 	string getNomJoueur();
 	void arreterSur(int);
 	/* calcul loyer
-	 * pour pouvoir faire différence batiment non batiment,
-	 * hypothèque ou non
-	 * tout les terrains de la même couleur ??
+	 * pour pouvoir faire diffÃ©rence batiment non batiment,
+	 * hypothÃ¨que ou non
+	 * tout les terrains de la mÃªme couleur ??
 	 * permet aussi de faire varier prix selon
 	 * nb de gare, nb de compagnie de service public
 	 *
 	 *
 	 *  */
 };
-
-
-
 
 #endif /* PROPRIETE_H_ */
