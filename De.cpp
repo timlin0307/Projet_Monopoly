d6f366ff518f27fc1a15c3dcfeca1ecd52a1a550
Sasaki Yamato <54052564+timlin0307@users.noreply.github.com>
2022-01-18 15:21:54 +0100

    Update and rename de.cpp to De.cpp

---
+++ De.cpp
@@ -0,0 +1,25 @@
+/*
+ * De.cpp
+ *
+ *  Created on: 30 déc. 2021
+ *      Author: basti
+ */
+
+#include "De.h"
+
+De::de() {
+	nbFaces = 6 ;
+	this->valeur = 1 + rand() % 6;
+}
+De::de(int nbFaces){
+	this->nbFaces = nbFaces;
+	valeur = 1 + rand() % nbFaces;
+}
+
+int De::getNbfaces(){
+	return nbFaces;
+}
+
+int De::getValeur(){
+	return valeur;
+}
