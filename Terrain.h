f2a939256487aba342c33a6ae251dfa3ccee2188
Sasaki Yamato <54052564+timlin0307@users.noreply.github.com>
2022-01-18 15:28:28 +0100

    Update Terrain.h

--- Terrain.h
+++ Terrain.h
@@ -1,7 +1,7 @@
 /*
  * Terrain.h
  *
- *  Created on: 18 déc. 2021
+ *  Created on: 18 dÃ©c. 2021
  *      Author: lucas
  */
 
@@ -21,9 +21,8 @@ using namespace std;
  *
  */
 
-class Terrain : public Propriete
-{
-protected :
+class Terrain : public Propriete {
+    protected :
 	string couleur;
 	int * list_loyer; /* terrain nu , 1 , 2 ,3 ,4 maisons, hotel -> on sait que liste de dim 6*/
 	int prix_maison;
@@ -31,18 +30,13 @@ protected :
 	int nb_maisons; /*! construction uniforme*/ /* 5 maisons = 1 hotel */
 
 
-public :
-	Terrain(string nomcase, int loyer, int prixachat, string nomjoueur, int valhypo,string couleur,
-			int * list_loyer, int prix_maison, int prix_hotel,int nb_maisonsbool, bool hypotheque = false);
+    public :
+	Terrain(string nomcase, int loyer, int prixachat, string nomjoueur, int valhypo, string couleur,
+			int * list_loyer, int prix_maison, int prix_hotel, int nb_maisonsbool, bool hypotheque = false);
 	void arreterSur(int);
 	void calcul_loyer();
 	void set_NBmaisons(int);/* je pense on peut ne pas mettre d'argument et juste on ne peut augmenter que de 1 par 1 le nb de maison*/
 	int get_NBmaisons();
-
-
 };
-
-
-
 
 #endif /* TERRAIN_H_ */
